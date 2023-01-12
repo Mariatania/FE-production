15a0bddacefe1884d472c6b0479fe65fdad9ab4a
Tania <mariatania.renyaan@gmail.com>
2023-01-12 20:35:38 +0700

    first commit

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "https://final-project-fe-deployment.web.app" // TODO: replace this
+const studentName = "Maria Tania Renyaan" // TODO: replace this
+const studentId = "FE2927838" // TODO: replace this
+
+export { url, studentName, studentId }
